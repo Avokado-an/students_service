ec38fd3e0cffad6ddb8e2201c7a26fb9eee6925f
Anton.Bahdanau <c-abahdanau@neogov.net>
2022-06-09 15:53:44 +0400

    changes for default values

--- entity/filter.py
+++ entity/filter.py
@@ -1,10 +1,4 @@
-from entity.specialty import Specialty
-
-
 class StudentFilter:
-    average_mark = 0
-    subject = Specialty.none
-
     def __init__(self, average_mark, subject):
         self.average_mark = average_mark
         self.subject = subject
--- entity/student.py
+++ entity/student.py
@@ -1,13 +1,4 @@
-from specialty import Specialty
-
-
 class Student:
-    student_id = 0
-    name = ""
-    surname = ""
-    specialty = Specialty.none
-    average_mark = 0.
-
     def __init__(self, student_id, name, surname, specialty, average_mark):
         self.student_id = student_id
         self.name = name
